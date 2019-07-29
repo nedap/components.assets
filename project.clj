7449364efebbda0ca5f1f92093e46db8e1399116
vemv <vemv@users.noreply.github.com>
2019-07-30 00:38:07 +0200

    Adopt speced.def

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   ;; Please keep the dependencies sorted a-z.
   :dependencies [[com.nedap.staffing-solutions/stefon "0.5.0"
                   :exclusions [clj-time commons-codec com.fasterxml.jackson.core/jackson-core]]
-                 [com.nedap.staffing-solutions/utils.io "0.1.1"
-                  :exclusions [com.nedap.staffing-solutions/utils.spec org.apache.commons/commons-compress]]
-                 [com.nedap.staffing-solutions/utils.modular "0.4.0"]
+                 [com.nedap.staffing-solutions/utils.io "1.0.0"
+                  :exclusions [org.apache.commons/commons-compress]]
+                 [com.nedap.staffing-solutions/utils.modular "2.0.0"]
                  [com.stuartsierra/component "0.4.0"]
                  [garden "1.3.5"]
                  [org.clojure/clojure "1.10.1"]
@@ -49,11 +49,10 @@
   ;; NOTE: deps marked with #_"transitive" are there to satisfy the `:pedantic?` option.
   :profiles {:dev {:dependencies [[cider/cider-nrepl "0.16.0" #_"formatting-stack needs it"]
                                   [com.clojure-goes-fast/clj-java-decompiler "0.2.1"]
-                                  [com.nedap.staffing-solutions/utils.spec "0.9.0"]
                                   [com.stuartsierra/component "0.4.0"]
                                   [com.taoensso/timbre "4.10.0"]
                                   [criterium "0.4.4"]
-                                  [formatting-stack "0.19.3"
+                                  [formatting-stack "1.0.0-alpha3"
                                    :exclusions [rewrite-clj]]
                                   [lambdaisland/deep-diff "0.0-29"]
                                   [medley "1.1.0"]
@@ -70,4 +69,4 @@
              :ci  {:pedantic?    :abort
                    :jvm-opts     ["-Dclojure.main.report=stderr"]
                    :global-vars  {*assert* true} ;; `ci.release-workflow` relies on runtime assertions
-                   :dependencies [[com.nedap.staffing-solutions/ci.release-workflow "1.1.0"]]}})
+                   :dependencies [[com.nedap.staffing-solutions/ci.release-workflow "1.3.0-alpha3"]]}})
