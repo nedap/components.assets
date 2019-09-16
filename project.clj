ac4ac5affba84d1c08b4d22525918ce4445c7d6c
vemv <vemv@users.noreply.github.com>
2019-09-16 12:15:17 +0200

    Bump ncrw

--- project.clj
+++ project.clj
@@ -71,4 +71,4 @@
              :ci  {:pedantic?    :abort
                    :jvm-opts     ["-Dclojure.main.report=stderr"]
                    :global-vars  {*assert* true} ;; `ci.release-workflow` relies on runtime assertions
-                   :dependencies [[com.nedap.staffing-solutions/ci.release-workflow "1.3.0-alpha3"]]}})
+                   :dependencies [[com.nedap.staffing-solutions/ci.release-workflow "1.4.0"]]}})
