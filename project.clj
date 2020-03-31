105778ad7973f61da38860ce986be1a31cf628a2
vemv <vemv@users.noreply.github.com>
2020-03-31 18:25:26 +0200

    Bump com.nedap.staffing-solutions/stefon
    No functional changes.
    
    This makes `components.assets` not depend on private Maven repos.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 ;; Please don't bump the library version by hand - use ci.release-workflow instead.
 (defproject com.nedap.staffing-solutions/components.assets "2.1.0"
   ;; Please keep the dependencies sorted a-z.
-  :dependencies [[com.nedap.staffing-solutions/stefon "0.5.1"
+  :dependencies [[com.nedap.staffing-solutions/stefon "0.5.2"
                   :exclusions [clj-time commons-codec com.fasterxml.jackson.core/jackson-core]]
                  [com.nedap.staffing-solutions/utils.io "1.0.0"
                   :exclusions [com.nedap.staffing-solutions/speced.def
